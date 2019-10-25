e51be27b6123be2e4121e502b0723fe174481a04
arigl <42983251+arigl@users.noreply.github.com>
2019-10-24 21:19:14 -0700

    extensions comments

--- app/src/main/java/com/example/anythingisfine/util/ViewExtensions.kt
+++ app/src/main/java/com/example/anythingisfine/util/ViewExtensions.kt
@@ -2,10 +2,14 @@ package com.example.anythingisfine.util
 
 import android.view.View
 
+// these functions offer nice visual features that add appeal to the application
+
+// this function causes an object to turn invisible if it helps improve the visual fidelity of the application
 fun View.toggleVisibility() {
     visibility = if (visibility == View.VISIBLE) {View.INVISIBLE} else { View.VISIBLE }
 }
 
+// this is a nice feature that rotates an image each time a button is pressed. it gives a visual appeal each time you click
 fun View.rotate90(){
     rotation = (rotation + 90) % 360
 }
