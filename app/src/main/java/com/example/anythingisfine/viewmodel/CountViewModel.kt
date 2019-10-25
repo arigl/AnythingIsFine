69f83210fe52c171eb65b7a99e3c2b53327d9a26
arigl <42983251+arigl@users.noreply.github.com>
2019-10-24 23:06:08 -0700

    notes for countviewmodel

--- app/src/main/java/com/example/anythingisfine/viewmodel/CountViewModel.kt
+++ app/src/main/java/com/example/anythingisfine/viewmodel/CountViewModel.kt
@@ -5,6 +5,10 @@ import androidx.lifecycle.AndroidViewModel
 import com.example.anythingisfine.CountRepository
 
 class CountViewModel(application: Application) : AndroidViewModel(application) {
+    // Part of the Model View Model System layout
+    // Takes in a repository for an object oriented approach
+    // Initializes the functions for getting the user count to back up to a repository
+    // Initializes the function for setting the user count attached to a name to the repository 
     private val repository = CountRepository(application.applicationContext);
 
     fun getUserCount(name: String) = repository.getUserCount(name);
