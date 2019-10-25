9bff69e747f6b55e02a4015f516854bf6864c594
arigl <42983251+arigl@users.noreply.github.com>
2019-10-24 21:17:43 -0700

    Comments on the main activity

--- app/src/main/java/com/example/anythingisfine/ui/MainActivity.kt
+++ app/src/main/java/com/example/anythingisfine/ui/MainActivity.kt
@@ -12,28 +12,33 @@ import java.util.*
 class MainActivity : AppCompatActivity(){
     private lateinit var countViewModel: CountViewModel
 
-    private var whaleCounter: Long = 0
-    private fun getUsername() = intent.extras?.get("username").toString().toLowerCase(Locale.US)
+    private var whaleCounter: Long = 0 // stores the main counter value before initiation as a long 
+    private fun getUsername() = intent.extras?.get("username").toString().toLowerCase(Locale.US) // takes in the username from the login activity and formats it 
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // sets the activity to display on the content view 
 
-        countViewModel = ViewModelProviders.of(this).get(CountViewModel::class.java)
+        countViewModel = ViewModelProviders.of(this).get(CountViewModel::class.java) 
         countViewModel.getUserCount(getUsername()).observe(this, androidx.lifecycle.Observer{updateCounter(it)})
 
         myButton.setOnClickListener{
+            // when the button is clicked, it increases the value of the counter and then saves it on the corresponding username
             countViewModel.setUserCount(getUsername(), whaleCounter + 1)
+            
+            // Exectutes a method that rotates the image for visual appeal 
             myImage.rotate90()
         }
 
         restartButton.setOnClickListener{
+            // allows a user to restart their counter without having to change usernames 
             whaleCounter = 0;
             CounterText.setText(""+whaleCounter);
         }
     }
 
     private fun updateCounter(count: Long){
+        // updates the counter and converts the text to the new value 
         whaleCounter = count
         CounterText.text = whaleCounter.toString()
     }
